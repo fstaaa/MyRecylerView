9f400854d54e101dcc1d582eb3f7486f97ea5afa
fstaaa <68883246+fstaaa@users.noreply.github.com>
2020-11-10 19:55:51 +0700

    first commit

---
+++ app/src/main/java/com/example/myrecyclerview/Hero.kt
@@ -0,0 +1,40 @@
+package com.example.myrecyclerview
+
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Hero(
+        var name: String,
+        var description: String,
+        var photo: String
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeString(photo)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Hero> {
+        override fun createFromParcel(parcel: Parcel): Hero {
+            return Hero(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Hero?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+annotation class Parcelize
